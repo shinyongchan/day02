3656f574e9d290f462405c91f6d11bf032e50139
syc <tlsdydcks135@naver.com>
2023-01-13 13:35:55 +0900

    day02_03

--- day02.py
+++ day02.py
@@ -1,3 +1,12 @@
-PI=3.14
-print(f'원주율의 값은 {PI}이고 자료형은 {type(PI)}입니다.')
-
+print(2**5)
+print(pow(2,5))
+print(divmod(9,5))
+print(type(divmod(9,5)))
+print(type((1,2)))
+test=1,2 #packing
+print(type(test))
+print(test)
+print(test[1])
+a,b=test #unpacking
+print(a)
+print(b)
